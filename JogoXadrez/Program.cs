6bb83801f45af8ee82af2f03d9d1c785357c7559
ItamarHavenstein <itamarhavenstein@hotmail.com>
2021-10-05 21:55:18 -0300

    Melhorando a implementação do metodo imprimir o tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -8,13 +8,20 @@ namespace JogoXadrez
   {
     static void Main(string[] args)
     {
+      try{
+      Tabuleiro tab = new Tabuleiro(8, 8);
+      tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+      tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+      tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-      PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-      Console.WriteLine(pos);
-      Console.WriteLine(pos.ToPosicao());
+      tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
 
+      Tela.ImprimirTabuleiro(tab);
       Console.ReadLine();
-
+      }
+      catch(TabuleiroException e){
+        Console.WriteLine(e.Message);
+      }
     }
   }
 }
--- JogoXadrez/Tela.cs
+++ JogoXadrez/Tela.cs
@@ -9,6 +9,7 @@ namespace JogoXadrez
     {
       for (int i = 0; i < tab.Linhas; i++)
       {
+        Console.Write(8 - i + " ");
         for (int j = 0; j < tab.Colunas; j++)
         {
           if (tab.Parts(i, j) == null)
@@ -17,10 +18,26 @@ namespace JogoXadrez
           }
           else
           {
-            Console.Write(tab.Parts(i, j) + " ");
+            ImprimirPeca(tab.Parts(i,j));
+            Console.Write(" ");
           }
         }
         Console.WriteLine();
+      }
+      Console.WriteLine("  a b c d e f g h");
+    }
+    public static void ImprimirPeca(Peca peca)
+    {
+      if (peca.Cor == Cor.Branca)
+      {
+        Console.Write(peca);
+      }
+      else
+      {
+        ConsoleColor aux = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(peca);
+        Console.ForegroundColor = aux;
       }
     }
   }
